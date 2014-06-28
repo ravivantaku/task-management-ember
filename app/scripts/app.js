ce79a416e6ca1074467e76c470e84471c06703f2
ravi v <vravimca2011@gmail.com>
2014-06-28 15:20:12 +0530

    add app.js file

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+var Taskmanagement = window.Taskmanagement = Ember.Application.create({
+    LOG_TRANSITION: true
+});
+
+/* Order and include as you please. */
+require('scripts/controllers/*');
+require('scripts/store');
+require('scripts/models/*');
+require('scripts/routes/*');
+require('scripts/components/*');
+require('scripts/views/*');
+require('scripts/router');
