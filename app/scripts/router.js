10cc59ca7a535b6eea62245db460724e5225fd94
ravi v <vravimca2011@gmail.com>
2014-07-02 17:49:00 +0530

    add singup,newtask,showtask,edittask routes

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,4 +1,12 @@
 Taskmanagement.Router.map(function () {
   // Add your routes here
-    this.resource('tasks');
+    this.route('signup');
+    this.resource('tasks', function() {
+//        this.route('new');
+//        this.resource('task', {path: '/:id'});
+//        this.route('edit', {path: '/:id/edit'})
+    });
+    this.route('new', {path:'/tasks/new'});
+    this.resource('show', {path: '/tasks/:id'});
+    this.route('edit', {path:'/tasks/:id/edit'});
 });
